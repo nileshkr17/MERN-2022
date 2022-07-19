e8dd2ffa82a8f003d299646901d7b1434ead1cae
Nilesh <ece.20becd76@silicon.ac.in>
2022-07-20 00:46:09 +0530

    [added-a9-n]

--- check/Cart.js
+++
@@ -1,9 +0,0 @@
-function Cart(id,productName,productPrice,productDesc,productDateOfPurchase,productImage){
-    this.id = id;
-    this.productName = productName;
-    this.productPrice = productPrice;
-    this.productDesc = productDesc;
-    this.productDateOfPurchase = productDateOfPurchase;
-    this.productImage = productImage;
-} 
-exports.Cart = Cart;
