0cc878271355fbbaeb57b2859c8e7da16a837d4c
Nilesh <ece.20becd76@silicon.ac.in>
2022-07-22 00:15:20 +0530

    [create-react-app]

--- ReactDay01/cart-project/src/App.js
+++ ReactDay01/cart-project/src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
+import HelloWorld from './components/HelloWorld';
 
 function App() {
   return (
     <div className="App">
         <h1>Mern Stack Course</h1>
+        <HelloWorld/>
     </div>
   );
 }
 
 export default App;
+//functional component - a js function
+//class component
---
+++ ReactDay01/cart-project/src/components/HelloWorld.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function HelloWorld (){
+    return(
+        <div>
+           <h1>Hello World</h1>
+        </div>
+    );
+}///jsx part
+ 
+export default HelloWorld
