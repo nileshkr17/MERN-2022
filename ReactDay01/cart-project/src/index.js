4b3c726df97c98f9e4c120d61adad8d3181adf96
Nilesh <ece.20becd76@silicon.ac.in>
2022-07-22 00:04:58 +0530

    [create-react-app]

--- ReactDay01/cart-project/src/index.js
+++ ReactDay01/cart-project/src/index.js
@@ -5,11 +5,11 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render(             //presenting componets 
   <React.StrictMode>
-    <App />
+    <App />           
   </React.StrictMode>
-);
+);//root componet of our project
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
