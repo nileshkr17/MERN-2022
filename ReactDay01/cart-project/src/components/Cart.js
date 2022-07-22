7c5334fe9444667b3d0a92e16a47a740ab1b59b6
Nilesh <ece.20becd76@silicon.ac.in>
2022-07-22 08:33:54 +0530

    [cart.css]

--- ReactDay01/cart-project/src/components/Cart.js
+++ ReactDay01/cart-project/src/components/Cart.js
@@ -1,15 +1,27 @@
 import React  from "react";
 import './Cart.css';
 export const Cart = ()=>{
+      
+            const cart ={
+                ProductName:"Laptop",
+                ProductImg:"https://www.rlogical.com/wp-content/uploads/2020/12/MERN-Stack-considered-the-Best-for-Developing-Web-Apps.png",
+                ProductPrice:'10',
+                ItemCompany:'Mac Book Pro M1'
+
+                    }
     return(
     <div>
          <div className='CartContainer'>
-            <div className='CartTitle'>Cart Item</div>
+            <div className='CartTitle'>{cart.ProductName}</div>
             <div className='CartImg'>
-                <img src="https://www.rlogical.com/wp-content/uploads/2020/12/MERN-Stack-considered-the-Best-for-Developing-Web-Apps.png" alt="description of image"/>
+                <img src={cart.ProductImg} alt="description of image"/>
             </div>
-            <div className='CartPrice'>100rs</div>
-            <div className='ItemCompany'>Mac Book Pro M1</div>
+            <div className='CartPrice'>{cart.ProductPrice}</div>
+            <div className='ItemCompany'>{cart.ItemCompany}</div>
+            <div className="BuyButtonContainer">
+                <button className="BuyNow">Buy Now</button>
+               
+        </div>
         </div>
 
      </div>
