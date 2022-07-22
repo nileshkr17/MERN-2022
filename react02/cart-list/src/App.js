924832fedb6c168d7996de3c0e20df289932dee5
Nilesh <ece.20becd76@silicon.ac.in>
2022-07-23 01:45:14 +0530

    [all added]

--- react02/cart-list/src/App.js
+++ react02/cart-list/src/App.js
@@ -22,21 +22,23 @@ function App() {
       ProductPrice:'$10',
       ProductRating:'Must Watch!'
     },
+    { 
+      ProductName:"Breaking Bad",
+      ProductImg:"https://assets.gadgets360cdn.com/pricee/assets/product/202203/BreakingBad-4_1647286045.jpg",
+      ProductPrice:'$10',
+      ProductRating:'Must Watch!'
+      
+    },
     {
+      
       ProductName:"Deathnote",
       ProductImg:"https://flxt.tmsimg.com/assets/p225665_b_v9_ab.jpg",
       ProductPrice:'$10',
       ProductRating:'Must Watch!'
     },
     {
-      ProductName:"Happiness",
-      ProductImg:"https://i.pinimg.com/736x/61/96/12/6196129834dedd3ace510d49e6a8f0a8.jpg",
-      ProductPrice:'$10',
-      ProductRating:'Must Watch!'
-    },
-    {
-      ProductName:"Happiness",
-      ProductImg:"https://i.pinimg.com/736x/61/96/12/6196129834dedd3ace510d49e6a8f0a8.jpg",
+      ProductName:"RRR",
+      ProductImg:"https://pbs.twimg.com/profile_images/1513422368357912577/6FgmBymm_400x400.jpg",
       ProductPrice:'$10',
       ProductRating:'Must Watch!'
     }
@@ -48,6 +50,8 @@ function App() {
       <Cart ProductName={cartArr[1].ProductName} ProductImg={cartArr[1].ProductImg} ProductPrice={cartArr[1].ProductPrice} ProductRating={cartArr[1].ProductRating}/>
       <Cart ProductName={cartArr[2].ProductName} ProductImg={cartArr[2].ProductImg} ProductPrice={cartArr[2].ProductPrice} ProductRating={cartArr[2].ProductRating}/>
       <Cart ProductName={cartArr[3].ProductName} ProductImg={cartArr[3].ProductImg} ProductPrice={cartArr[3].ProductPrice} ProductRating={cartArr[3].ProductRating}/>
+      <Cart ProductName={cartArr[4].ProductName} ProductImg={cartArr[4].ProductImg} ProductPrice={cartArr[4].ProductPrice} ProductRating={cartArr[4].ProductRating}/>
+      <Cart ProductName={cartArr[5].ProductName} ProductImg={cartArr[5].ProductImg} ProductPrice={cartArr[5].ProductPrice} ProductRating={cartArr[5].ProductRating}/>
       <Cart ProductName={cartArr[4].ProductName} ProductImg={cartArr[4].ProductImg} ProductPrice={cartArr[4].ProductPrice} ProductRating={cartArr[4].ProductRating}/>
       <Cart ProductName={cartArr[5].ProductName} ProductImg={cartArr[5].ProductImg} ProductPrice={cartArr[5].ProductPrice} ProductRating={cartArr[5].ProductRating}/>
     </div>
